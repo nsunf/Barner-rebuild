414d1989f2454eb9053f88ae27998dad1d9d25a3
nsunf <jun5013@naver.com>
2022-01-23 11:17:52 +0900

    add card5, data models

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+const { decode } = require('querystring');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('public/javascripts/glasses_products_data.json', 'utf-8', (err,data) => {
+    let decodedData = JSON.parse(data);
+    res.render('index', { title: 'Barner', glasses_data: decodedData });
+  })
 });
 
 module.exports = router;
