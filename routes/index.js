aeb8e37c41cdf8960895cdf58fa797eb1b951f6d
nsunf <jun5013@naver.com>
2022-01-24 11:31:11 +0900

    add sunglasses section from card1 to card4

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,12 @@ const { decode } = require('querystring');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  fs.readFile('public/javascripts/glasses_products_data.json', 'utf-8', (err,data) => {
-    let decodedData = JSON.parse(data);
-    res.render('index', { title: 'Barner', glasses_data: decodedData });
+  fs.readFile('public/javascripts/glasses_products_data.json', 'utf-8', (err, glasses_data) => {
+    fs.readFile('public/javascripts/sunglasses_products_data.json', 'utf-8', (err, sunglasses_data) => {
+      let glassesDecodedData = JSON.parse(glasses_data);
+      let sunglassesDecodedData = JSON.parse(sunglasses_data);
+      res.render('index', { title: 'Barner', glasses_data: glassesDecodedData, sunglasses_data: sunglassesDecodedData });
+    })
   })
 });
 
